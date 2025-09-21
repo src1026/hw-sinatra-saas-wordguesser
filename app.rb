c03eac1f6fbceba0bce4535d25bcd0b698e31a3b
Shuyan <sc5144@barnard.edu>
2025-09-21 19:18:24 -0400

    Implement Sinatra web interface for Wordguesser game

--- app.rb
+++ app.rb
@@ -39,8 +39,17 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    letter = params[:guess].to_s[0]
+    
+    begin
+      result = @game.guess(letter)
+      if result == false
+        flash[:message] = "You have already used that letter."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+    
     redirect '/show'
   end
 
@@ -50,8 +59,16 @@ class WordGuesserApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    game_status = @game.check_win_or_lose
+    
+    case game_status
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show
+    end
   end
 
   get '/win' do
